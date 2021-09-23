491ccab2415fbf1854a5699914ce3190bd6b11f2
Felicity Brammer <felicitybrammer@gmail.com>
2021-09-23 10:16:59 -0700

    display questions and answer buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,39 @@
 var introEl = document.querySelector("#intro-container");
-// var startButtonEl = document.querySelector("#start-btn");
+var startButtonEl = document.createElement('button');
 var quizContainerEl = document.querySelector("#quiz-container");
 var timerEl = document.querySelector("#countdown");
+var answerContainerEl = document.querySelector("#answer-container");
 var score = 0;
 var timeLeft;
+var correctMessage = document.createElement('span').textContent = "Correct!";
+var wrongMessage = document.createElement('span').textContent = "Wrong!";
 
 
 var questions = [
     {
         question: "What is the correct way to write a JavaScript array?",
         choices: ["var colors = ['red', 'green', 'blue']", "var colors = 'red', 'green', 'blue'", "var colors = (1:'red', 2:'green', 3:'blue'", "var colors = 1 = ('red'), 2 = ('green'), 3 = ('blue')"],
-        correctAnswer: "var colors = ['red', 'green', 'blue']" //or choices[0]
+        correctAnswer: "var colors = ['red', 'green', 'blue']" 
     },
     {
         question: "Inside which HTML element do we put JavaScript?",
         choices: ["<js>", "<script>", "<scripting>", "<javascript>"],
-        correctAnswer: "<script>" //choices[1]
+        correctAnswer: "<script>" 
     },
     {
         question: "A very useful tool during development and debugging for printing content to the debugger is:",
         choices: ["JavaScript", "Terminal/Bash", "console.log", "for loops"],
-        correctAnswer: "console.log" //choices[2]
+        correctAnswer: "console.log" 
     },
     {
         question: "Arrays in JavaScript can be used to store ______________.",
         choices: ["Numbers", "Strings", "Objects", "All of the above"],
-        correctAnswer: "All of the above" //choices[3]
+        correctAnswer: "All of the above" 
     },
     {
         question: "What is the correct JavaScript syntax to change the content of this HTML element? <p id='demo'>This is a demonstration.</p>",
         choices: ["#demo.innerHTML = 'Hello World!';", "document.getElementByName('p').innerHTML = 'Hello World!';", "document.getElementById('demo').innerHTML = 'Hello World!';", "document.getElement('p').innerHTML = 'Hello World!';"],
-        correctAnswer: "document.getElementById('demo').innerHTML = 'Hello World!';" //choices[2]
+        correctAnswer: "document.getElementById('demo').innerHTML = 'Hello World!';" 
     }
 ];
 var removeAllChildNodes = function(parent) {
@@ -47,32 +50,59 @@ var startQuiz = function() {
    for (var i = 0; i < questions.length; i++) {
        //determine which question to show (collect a question object)
        //var nextQuestion = questions[i];    //Math.floor(Math.random() * questions.length)]; 
-       showQuestion(questions[i]);
+       showQuestion(i);
+        
    } 
    
 }; 
 
-var showQuestion = function() {
+var showQuestion = function(i) {
     
-       
     //dynamically create element, append to page, 
-    //display question value from  question object  array[i].question
-    //display choices as buttons array[i].choices
+    var questionEl = document.createElement('h2');
+    var button1El = document.createElement('button');
+    var button2El = document.createElement('button');
+    var button3El = document.createElement('button');
+    var button4El = document.createElement('button');
 
-  
+    //questions.q as text
+    questionEl.textContent = questions[i].question;
+    //questions.choices[i] as list?
+    button1El.textContent = questions[i].choices[0];
+    button2El.textContent = questions[i].choices[1];
+    button3El.textContent = questions[i].choices[2];
+    button4El.textContent = questions[i].choices[3];
+    //append to quizContainerEl
+    quizContainerEl.appendChild(questionEl);
+    answerContainerEl.appendChild(button1El);
+    answerContainerEl.appendChild(button2El);
+    answerContainerEl.appendChild(button3El);
+    answerContainerEl.appendChild(button4El);
+    quizContainerEl.appendChild(answerContainerEl);
+    //display question value from  question object  array[i].question
+    //display choices as buttons array[i].choices 
 
 };
 
-var checkAnswer = function() {
-    //if correct, score++, display message, run showQuestions (if value not already used), 
+var checkAnswer = function(event) {
+
+    if (event.target.matches(questions.correctAnswer)) { //if button pressed text matches correct answer text
+        score++;
+        answerContainerEl.appendChild(correctMessage);
+    }
+    //if correct, score++, display message, run showQuestions 
+
+    
     //if no more questions, endQuiz()
     //if incorrect, wrongAnswer()
+    //clear page
 };
 
 var wrongAnwer = function() {
     //display message
     //check timer to see if enough time to deduct for wrong answer
     //if yes, deduct time
+    //clear page
     // show questions
 };
 
@@ -81,7 +111,7 @@ var endQuiz = function() {};
 
 
  function countDown() { 
-     var timeLeft = 60;
+     var timeLeft = 75;
 
      var timeInterval = setInterval(function() {
         if (timeLeft > 0) {
@@ -98,7 +128,7 @@ var displayIntro = function() {
     //add title, paragraph and button
     var introTitleEl = document.createElement('h1');
     var introParaEl = document.createElement('p');
-    var startButtonEl = document.createElement('button');
+    //var startButtonEl = document.createElement('button');
 
     introTitleEl.textContent = 'Coding Quiz Challenge';
     introParaEl.textContent = 'Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will decrease your remaining time by five seconds.';
@@ -114,5 +144,6 @@ var displayIntro = function() {
 
 displayIntro();
 
-startButtonEl.addEventListener('click', startQuiz());
-//event listener for answer button calls checkAnswer
+startButtonEl.addEventListener('click', startQuiz);
+//event listener for answer button group calls checkAnswer
+answerContainerEl.addEventListener('click', checkAnswer);
